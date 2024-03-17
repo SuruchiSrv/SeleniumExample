5cf36e49656c4d49a37737fba35ebf0b22b4b2d4
SuruchiSrv <autotesting24@gmail.com git config --global user.name>
2024-03-17 09:02:06 +0000

    selenium dependencies added

---
+++ src/main/java/selTry/Edge.java
@@ -0,0 +1,18 @@
+package selTry;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Edge
+{
+	
+		    public static void main(String[] args)
+		    { 
+	       EdgeDriver driver = new EdgeDriver();
+
+	        driver.get("https://selenium.dev");
+
+	        //driver.quit();
+
+}}
---
+++ src/main/java/selTry/Example1.java
@@ -0,0 +1,18 @@
+package selTry;
+
+
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+
+	public class Example1   {
+	    public static void main(String[] args) {
+	        WebDriver driver = new ChromeDriver();
+
+	        driver.get("https://selenium.dev");
+
+	        //driver.quit();
+	
+
+	}
+
+}
